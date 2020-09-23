e37a89e191f721756c2ab83ba37e5636f82f034c
lefur <samuel.lefur@hotmail.fr>
2020-09-23 15:40:10 +0200

    Première version du projet...

---
+++ calcul.php
@@ -0,0 +1,11 @@
+<?php
+
+$transport = $_POST['transport'];
+$distance = $_POST['distance'];
+$polution = $distance*$transport;
+
+print("Bonjour votre trajet en $transport dégage $pollution gCO2  ");
+
+
+
+ ?>
